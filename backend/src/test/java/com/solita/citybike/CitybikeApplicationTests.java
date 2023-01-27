790a9ce65306471023dff5f6f622e5d28e244fdc
hevipiude <veerapir@hotmail.com>
2023-01-27 20:05:45 +0200

    setup frontend & backend

---
+++ backend/src/test/java/com/solita/citybike/CitybikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solita.citybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
