92a6e6ba593ddba83c89d9fd8299401204fdfb6b
hevipiude <veerapir@hotmail.com>
2023-01-27 20:48:45 +0200

    id for entity Journey added

--- backend/src/main/java/com/solita/citybike/Journey.java
+++ backend/src/main/java/com/solita/citybike/Journey.java
@@ -3,9 +3,15 @@ package com.solita.citybike;
 import java.security.Timestamp;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class Journey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     private Timestamp departure_time;
 
