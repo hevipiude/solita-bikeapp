13f098d7fe465d9c3dd0f3aeb66f01e5cb3dabb6
hevipiude <veerapir@hotmail.com>
2023-01-28 14:44:45 +0200

    controller for paginated Journeys added

---
+++ backend/src/main/java/com/solita/citybike/JourneyController.java
@@ -0,0 +1,23 @@
+package com.solita.citybike;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// The JourneyController class handles HTTP requests with @RestController annotation. It uses @Autowired to inject a JourneyService instance variable.
+@RestController
+public class JourneyController {
+
+    @Autowired
+    private JourneyService journeyService;
+
+    // The class has a @GetMapping("/journeys") method which returns paginated data
+    // of type Journeys. The method calls the journeyService's getAllJourneys()
+    // method with a Pageable object as parameter for pagination.
+    @GetMapping("/journeys")
+    public Page<Journeys> getAllJourneys(Pageable pageable) {
+        return journeyService.getAllJourneys(pageable);
+    }
+}
