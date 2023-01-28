8d6e786a194abfba5ba9bdfe1348bbc0a8092d73
hevipiude <veerapir@hotmail.com>
2023-01-28 17:04:52 +0200

    greeting test removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,15 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import "./App.css"
+import JourneyTable from "./components/JourneyTable"
 
 function App() {
-  const [message, setMessage] = useState({ id: 0, content: "" })
-  const url = "http://localhost:8080/greeting?name=Taateli"
-
-  useEffect(() => {
-    axios
-      .get(url)
-      .then((response) => {
-        setMessage(response.data)
-      })
-      .catch((error) => {
-        console.log("error : ")
-        console.log(error)
-      })
-  }, [])
-
   return (
     <div className="App">
-      <h1>
-        {message.id} {message.content}
-      </h1>
-      {console.log(message.id, message.content)}
+      <h1>tabletest</h1>
+      <div>
+        <JourneyTable />
+      </div>
     </div>
   )
 }
