3fab42c1318b13621d565550eb4bc347cc1c528e
hevipiude <veerapir@hotmail.com>
2023-01-28 15:27:22 +0200

    improper imports fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,28 @@
-import { useState, useEffect } from 'react'
-import axios from "axios";
-import './App.css'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
-  const [message, setMessage] = useState({id:0, content:""})
-  const url = 'http://localhost:8080/greeting?name=Taateli';
+  const [message, setMessage] = useState({ id: 0, content: "" })
+  const url = "http://localhost:8080/greeting?name=Taateli"
 
-    useEffect(() => {
-     axios.get(url).then(response => {
-            setMessage(response.data);
-        }).catch(error => {
-            console.log('error : ');
-            console.log(error);
-        });
-    
-  }, []);
-       
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setMessage(response.data)
+      })
+      .catch((error) => {
+        console.log("error : ")
+        console.log(error)
+      })
+  }, [])
 
   return (
     <div className="App">
-      <h1>{message.id} {message.content}</h1>
+      <h1>
+        {message.id} {message.content}
+      </h1>
       {console.log(message.id, message.content)}
     </div>
   )
