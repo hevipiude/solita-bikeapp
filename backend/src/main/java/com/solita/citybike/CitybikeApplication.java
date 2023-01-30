ae526d87ef6f992bf95d7aeca7db873c4697db3a
hevipiude <veerapir@hotmail.com>
2023-01-30 16:21:54 +0200

    path for mapping changed

--- backend/src/main/java/com/solita/citybike/CitybikeApplication.java
+++ backend/src/main/java/com/solita/citybike/CitybikeApplication.java
@@ -18,7 +18,7 @@ public class CitybikeApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/").allowedOrigins("http://localhost:8080");
 			}
 		};
 	}
