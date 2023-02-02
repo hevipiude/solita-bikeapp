c2131f8eb5998d8364ff4a9f5a8348e4b59e58af
hevipiude <veerapir@hotmail.com>
2023-02-02 16:00:02 +0200

    router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import JourneyTable from './components/JourneyTable'
+import NavBar from './components/NavBar'
+import StationDetails from './components/StationDetails'
 import StationTable from './components/StationTable'
 
 function App() {
   return (
     <div className='App'>
-      <h1>tabletest</h1>
-      <div>
-        <StationTable />
-        <JourneyTable />
-      </div>
+      <Router>
+        <NavBar />
+        <div>
+          <Routes>
+            <Route path='/journeys' element={<JourneyTable />} />
+            <Route path='/stations' element={<StationTable />} />
+            <Route path='/s_station' element={<StationDetails />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
