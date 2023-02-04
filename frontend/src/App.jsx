878882026191a380660ba1d2f45af5e51ca05167
hevipiude <veerapir@hotmail.com>
2023-02-04 11:32:04 +0200

    changed route for journeys

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import JourneyTable from './components/JourneyTable'
 import NavBar from './components/NavBar'
-import StationDetails from './components/StationDetails'
 import StationTable from './components/StationTable'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './themes/theme'
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <NavBar />
-        <div>
-          <Routes>
-            <Route path='/journeys' element={<JourneyTable />} />
-            <Route path='/stations' element={<StationTable />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Router>
+          <NavBar />
+          <div>
+            <Routes>
+              <Route path='/' element={<JourneyTable />} />
+              <Route path='/stations' element={<StationTable />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 
