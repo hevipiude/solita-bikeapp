b37a59ee30e3ff69332e711bc5c1da0d34ba8f94
hevipiude <veerapir@hotmail.com>
2023-02-04 11:33:06 +0200

    added Map component for single station view

---
+++ frontend/src/components/CustomMap.jsx
@@ -0,0 +1,15 @@
+import { Map, Marker, ZoomControl } from 'pigeon-maps'
+
+function CustomMap({ y, x }) {
+  const position = [y, x]
+  const color = '#e57373'
+
+  return (
+    <Map width={800} height={280} defaultCenter={position} defaultZoom={16}>
+      <ZoomControl />
+      <Marker color={color} width={40} anchor={position} />
+    </Map>
+  )
+}
+
+export default CustomMap
